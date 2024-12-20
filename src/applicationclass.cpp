927731cb869d89f2208feec8761d699bc4e79bde
Sac ML <sachin191ml@gmail.com>
2024-12-19 17:05:18 -0800

    [RT-Unified] Handle D3DClass initialization failure gracefully with a message dialog box

--- src/applicationclass.cpp
+++ src/applicationclass.cpp
@@ -20,7 +20,12 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	bool result;
 	m_Direct3D = new D3DClass();
 	result = m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
-    return true;
+	if (!result)
+	{
+		MessageBox(hwnd, "Could not initialize Direct3D", "Error", MB_OK);
+		return false;
+	}
+	return true;
 }
 
 void ApplicationClass::Shutdown()
