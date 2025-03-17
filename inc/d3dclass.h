18c10ae93b756d86217ce3b97989c53338b1ba7e
Sac ML <sachin191ml@gmail.com>
2025-03-16 17:09:55 -0700

    Cleanup - untabified d3dclass .cpp and .h

--- inc/d3dclass.h
+++ inc/d3dclass.h
@@ -13,45 +13,45 @@ using namespace DirectX;
 
 class D3DClass {
 public:
-	D3DClass();
-	D3DClass(const D3DClass& from);
-	~D3DClass();
+    D3DClass();
+    D3DClass(const D3DClass& from);
+    ~D3DClass();
 
-	bool Initialize(int screenWidth, int screenHeight, bool vsync, HWND hwnd, bool fullscreen, float screenDepth, float screenNear);
-	void Shutdown();
+    bool Initialize(int screenWidth, int screenHeight, bool vsync, HWND hwnd, bool fullscreen, float screenDepth, float screenNear);
+    void Shutdown();
 
-	void BeginScene(float red, float green, float blue, float alpha);
-	void EndScene();
+    void BeginScene(float red, float green, float blue, float alpha);
+    void EndScene();
 
-	ID3D11Device* GetDevice();
-	ID3D11DeviceContext* GetDeviceContext();
+    ID3D11Device* GetDevice();
+    ID3D11DeviceContext* GetDeviceContext();
 
-	void GetProjectionMatrix(XMMATRIX&);
-	void GetWorldMatrix(XMMATRIX&);
-	void GetOrthoMatrix(XMMATRIX&);
+    void GetProjectionMatrix(XMMATRIX&);
+    void GetWorldMatrix(XMMATRIX&);
+    void GetOrthoMatrix(XMMATRIX&);
 
-	void GetVideoCardInfo(char*, int&);
+    void GetVideoCardInfo(char*, int&);
 
-	void D3DClass::SetBackBufferRenderTarget();
-	void D3DClass::ResetViewport();
+    void D3DClass::SetBackBufferRenderTarget();
+    void D3DClass::ResetViewport();
 
 private:
-	bool m_vsync_enabled;
-	int m_videoCardMemory;
-	char m_videoCardDescription[128];
+    bool m_vsync_enabled;
+    int m_videoCardMemory;
+    char m_videoCardDescription[128];
 
-	IDXGISwapChain* m_swapChain;
-	ID3D11Device* m_device;
-	ID3D11DeviceContext* m_deviceContext;
+    IDXGISwapChain* m_swapChain;
+    ID3D11Device* m_device;
+    ID3D11DeviceContext* m_deviceContext;
 
-	ID3D11RenderTargetView* m_renderTargetView;
-	ID3D11Texture2D* m_depthStencilBuffer;
-	ID3D11DepthStencilState* m_depthStencilState;
-	ID3D11DepthStencilView* m_depthStencilView;
-	ID3D11RasterizerState* m_rasterState;
+    ID3D11RenderTargetView* m_renderTargetView;
+    ID3D11Texture2D* m_depthStencilBuffer;
+    ID3D11DepthStencilState* m_depthStencilState;
+    ID3D11DepthStencilView* m_depthStencilView;
+    ID3D11RasterizerState* m_rasterState;
 
-	D3D11_VIEWPORT m_viewport;
-	XMMATRIX m_projectionMatrix;
-	XMMATRIX m_worldMatrix;
-	XMMATRIX m_orthoMatrix;
+    D3D11_VIEWPORT m_viewport;
+    XMMATRIX m_projectionMatrix;
+    XMMATRIX m_worldMatrix;
+    XMMATRIX m_orthoMatrix;
 };
